73a095f1de2e5817445b7fa9694f852d14311d2e
Sans11Pentium <sanskriti.ug22@nsut.ac.in>
2023-10-06 22:55:42 +0530

    files added

---
+++ simon.js
@@ -0,0 +1,81 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns=["yellow","red","purple","green"];
+let started=false;
+
+let level=0;
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game started");
+        started=true;
+        levelUp();
+    }
+});
+
+function buttonFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+};
+
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    //flash random button
+    let randomIndex=Math.floor(Math.random()*3);
+    let randColor=btns[randomIndex];
+    let randbtn=document.querySelector(`.${randColor}`);
+    // console.log(randomIndex);
+    // console.log(randColor);
+    // console.log(randbtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    console.log(userSeq);
+    buttonFlash(randbtn);
+};
+
+function checkAns(idx){
+    if(userSeq[idx]==gameSeq[idx]){
+        if(userSeq.length==gameSeq.length){
+            setTimeOut(levelUp,1000);
+        }
+    }else{
+        h2.innerHTML=`Game Over!Your score was <b>${level} <br> Press any key to restart`;
+        document.querySelector("body").style.backgroundColorcolor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColorcolor="white";
+        },150);
+        reset();
+    }
+}
+
+function btnPress(){
+    // console.log(this);
+    let btn=this;
+    buttonFlash(btn);
+
+    userColor=btn.getAttribute("id");
+    // console.log(userColor);
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+
+let allbtns=document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
